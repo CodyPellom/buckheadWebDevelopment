eb47f8259a5d28841e384fe66528915e8116e3b0
CodyPellom <paulpellom@gmail.com>
2021-01-03 12:44:44 -0500

    added vid file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  particlesJS.load('particles-js-2', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  particlesJS.load('particles-js-3', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
