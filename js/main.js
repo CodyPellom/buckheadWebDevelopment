ed33a272c0c841afc31ed21a14765e59c3a3192a
CodyPellom <paulpellom@gmail.com>
2021-01-11 21:12:43 -0500

    night wrap

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Initiate Particle */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
@@ -9,3 +10,11 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
   particlesJS.load('particles-js-3', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+/* Initiate Particle End */
+
+function getNavbarHeight() {
+    // debugger;;;
+    var navBar = document.getElementById('main-navbar-id');
+    var navbarHeight = navBar.clientHeight;
+    console.log(navbarHeight);
+};
