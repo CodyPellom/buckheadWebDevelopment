4deef44094f4f608ce30da8a041a2c5016001ae8
CodyPellom <paulpellom@gmail.com>
2021-02-14 16:56:41 -0500

    progress

--- main.js
+++ main.js
@@ -37,3 +37,15 @@ function navBarOpacityOnScroll() {
 }
 
 init();
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  });
