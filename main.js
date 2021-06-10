29570428a7d6d217bee78be46188ffe6a4519246
CodyPellom <paulpellom@gmail.com>
2021-06-10 10:30:23 -0400

    'yeah'

--- main.js
+++ main.js
@@ -12,17 +12,7 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
   });
 /* Initiate Particle End */
 
-function init() {
-  var me = this;
-  me.navBarOpacityOnScroll();
-};
 
-function getNavbarHeight() {
-    // debugger;;;
-    var navBar = document.getElementById('main-navbar-id');
-    var navbarHeight = navBar.clientHeight;
-    console.log(navbarHeight);
-};
 
 function navBarOpacityOnScroll() {
     var navBar = document.getElementById('main-navbar-id');
@@ -36,16 +26,56 @@ function navBarOpacityOnScroll() {
     })
 }
 
-init();
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+// Wrap every letter in a span
+// var textWrapper = document.querySelector('.ml6 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: false})
+//   .add({
+//     targets: '.ml6 .letter',
+//     translateY: ["1.1em", 0],
+//     translateZ: 0,
+//     duration: 750,
+//     delay: (el, i) => 50 * i
+//   });
 anime.timeline({loop: false})
   .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
+    opacity: 0,
+    duration: 500000,
+    easing: "easeOutExpo",
+    delay: 1000
   });
